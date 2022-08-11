5b7d00ee7b6c82bf6f10aea10c5956fb0a72bf5d
Hongyerim0 <dpfla927@naver.com>
2022-08-11 15:31:59 +0900

    출발역, 도착역, 현재시간, 요일 json형식으로

--- Input.java
+++ Input.java
@@ -4,18 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,11 +26,8 @@ import com.github.chrisbanes.photoview.PhotoView;
 public class Input extends AppCompatActivity {
     static String departure_text;
     static String arrival_text;
-
-    String formatedNow; //시간
-    char week; //요일
-
-    String jsonData;
+    static String formattedNow; //시간
+    static String week; //요일
 
     String text;
     static int count =0;
@@ -50,52 +44,25 @@ public class Input extends AppCompatActivity {
         intent = getIntent();
         text= intent.getStringExtra("selected_item"); //search 액티비티에서 역이름 받아오기
 
-        if(count==1) {
+        if(count==1) { //출발역
             departure_text = text;
             departure.setText(departure_text);
             count = 0;
             arrival.setText(arrival_text);
         }
-        else if(count==2){
+        else if(count==2){ //도착역
             departure.setText(departure_text);
             arrival_text = text;
             arrival.setText(arrival_text);
             count = 0;
         }
 
-        //요일
-        Date currentDate = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(currentDate);
-        int e = calendar.get(Calendar.DAY_OF_WEEK);
 
-        //시간
-
-        Date now = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("HH시 mm분");
-        formatedNow = formatter.format(now);
-
-        if (e == '2' || e == '3' || e == '4' || e == '5' || e == '6')
-            week = 'w';
-        else if (e == '7')
-            week = 'a';
-        else week = 'u';
-
-       /* Log.d("현재시간날짜","요일: "+week);
-        Log.d("현재시간날짜","now: "+formatedNow);*/
-
-
-        /*
-        text= intent.getStringExtra("selected_item"); //search 액티비티에서 역이름 받아오기
-        departure.setText(text); // 받아온 역이름 출발역 텍스트박스에 출력
-        arrival.setText(text); // 받아온 역이름 출발역 텍스트박스에 출력*/
-
-
+        ////////////////////////////////////////////////////////////////////////////////
 
         // 이미지 줌인 줌 아웃
         PhotoView photoView = findViewById(R.id.photoView);
         photoView.setImageResource(R.drawable.map2);
-
 
         // 출발역 텍스트박스 클릭시 액티비티 전환
         departure.setOnClickListener(new View.OnClickListener(){
@@ -117,14 +84,67 @@ public class Input extends AppCompatActivity {
             }
         });
 
-        //검색 버튼
+        //검색 버튼 클릭시 액티비티 전환
         Button search_button=(Button)findViewById(R.id.button1);
         search_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
+
+                //버튼 클릭하면 현재시간, 요일 구함
+                Date currentDate = new Date();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(currentDate);
+                int e = calendar.get(Calendar.DAY_OF_WEEK);
+
+                //시간
+                Date now = new Date();
+                SimpleDateFormat formatter = new SimpleDateFormat("HH시 mm분");
+                formattedNow = formatter.format(now);
+
+
+                if (e == '2' || e == '3' || e == '4' || e == '5' || e == '6')
+                    week = "w";
+                else if (e == '7')
+                    week = "a";
+                else week = "u";
+
+                Log.d("출발역","출발역: "+departure_text);
+                Log.d("도착역","도착역: "+arrival_text);
+                Log.d("현재시간날짜","now: "+ formattedNow);
+                Log.d("현재시간날짜","요일: "+week);
+
+
+                //////////////////////json 파싱/////////////////////////////////
+
+                ArrayList List =new ArrayList();
+                List.add(0,departure_text);
+                List.add(1,arrival_text);
+                List.add(2, formattedNow);
+                List.add(3,week);
+
+                Log.d("0번","0번: "+List.get(0));
+                Log.d("1번","1번: "+List.get(1));
+                Log.d("2번","2번: "+List.get(2));
+                Log.d("3번","3번: "+List.get(3));
+
+                JSONObject obj =new JSONObject();
+                try {
+                    obj.put("departure",List.get(0));
+                    obj.put("arrival",List.get(1));
+                    obj.put("now",List.get(2));
+                    obj.put("week",List.get(3));
+
+                    Log.d("json확인","확인: "+obj.toString());
+
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+
                 Intent intent = new Intent(getApplicationContext(), RouteTime.class);
                 startActivity(intent);
             }
         });
+
+
     }
 
 }
