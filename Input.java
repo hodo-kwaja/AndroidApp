17033c0468f4c038c1b4201e2588df617fac6da0
Hongyerim0 <dpfla927@naver.com>
2022-08-12 00:19:58 +0900

    json

--- Input.java
+++ Input.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -85,25 +86,26 @@ public class Input extends AppCompatActivity {
         });
 
         //검색 버튼 클릭시 액티비티 전환
-        Button search_button=(Button)findViewById(R.id.button1);
+        ImageButton search_button=(ImageButton) findViewById(R.id.button1);
         search_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
 
                 //버튼 클릭하면 현재시간, 요일 구함
+                // 시간
+                Date now = new Date();
+                SimpleDateFormat formatter = new SimpleDateFormat("HH시 mm분");
+                formattedNow = formatter.format(now);
+
+                //요일
                 Date currentDate = new Date();
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTime(currentDate);
                 int e = calendar.get(Calendar.DAY_OF_WEEK);
 
-                //시간
-                Date now = new Date();
-                SimpleDateFormat formatter = new SimpleDateFormat("HH시 mm분");
-                formattedNow = formatter.format(now);
-
-
-                if (e == '2' || e == '3' || e == '4' || e == '5' || e == '6')
+                //요일
+                if (e == 2 || e == 3 || e == 4 || e == 5 || e == 6)
                     week = "w";
-                else if (e == '7')
+                else if (e == 7)
                     week = "a";
                 else week = "u";
 
@@ -115,23 +117,18 @@ public class Input extends AppCompatActivity {
 
                 //////////////////////json 파싱/////////////////////////////////
 
-                ArrayList List =new ArrayList();
+              /*  ArrayList List =new ArrayList();
                 List.add(0,departure_text);
                 List.add(1,arrival_text);
                 List.add(2, formattedNow);
-                List.add(3,week);
-
-                Log.d("0번","0번: "+List.get(0));
-                Log.d("1번","1번: "+List.get(1));
-                Log.d("2번","2번: "+List.get(2));
-                Log.d("3번","3번: "+List.get(3));
+                List.add(3,week);*/
 
                 JSONObject obj =new JSONObject();
                 try {
-                    obj.put("departure",List.get(0));
-                    obj.put("arrival",List.get(1));
-                    obj.put("now",List.get(2));
-                    obj.put("week",List.get(3));
+                    obj.put("departure",departure_text);
+                    obj.put("arrival",arrival_text);
+                    obj.put("now",formattedNow);
+                    obj.put("week",week);
 
                     Log.d("json확인","확인: "+obj.toString());
 
